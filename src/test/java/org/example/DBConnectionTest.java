100a942a832c4950b8d350529ab429276f691171
Andjelina22 <stojkovic.andjelina29@gmail.com>
2026-06-14 15:37:25 +0200

    Dodati JUnit testovi

--- src/test/java/org/example/DBConnectionTest.java
+++ src/test/java/org/example/DBConnectionTest.java
@@ -4,7 +4,7 @@ import org.example.database.DBConnection;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+//Dodati JUnit testovi
 public class DBConnectionTest {
 
     @Test
